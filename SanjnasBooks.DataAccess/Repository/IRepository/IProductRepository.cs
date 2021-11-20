735460d3bf155564b54a0f2091755ab5b223a3da
Sanjna0769043 <W0769043@myscc.ca>
2021-11-20 17:29:49 -0500

    created a new Product repository under the Repository folfer

---
+++ SanjnasBooks.DataAccess/Repository/IRepository/IProductRepository.cs
@@ -0,0 +1,14 @@
+﻿using SanjnasBookStore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SanjnasBooks.DataAccess.Repository.IRepository
+{
+    interface IProductRepository : IRepository<Product>
+    {
+        void Update(ProductRepository product)
+    }
+}
