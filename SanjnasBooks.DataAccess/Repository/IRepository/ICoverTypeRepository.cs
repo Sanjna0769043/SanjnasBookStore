491e5681f44d459a7adfaac07ad6c401f60da7f7
Sanjna0769043 <W0769043@myscc.ca>
2021-11-20 13:37:54 -0500

    crate a IcoverType interface under the IRepository folder

---
+++ SanjnasBooks.DataAccess/Repository/IRepository/ICoverTypeRepository.cs
@@ -0,0 +1,14 @@
+﻿using SanjnasBooks.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SanjnasBooks.DataAccess.Repository.IRepository
+{
+    public interface ICoverTypeRepository : IRepository<coverType>
+    {
+        void Update(ICoverTypeRepository coverType);
+    }
+}
