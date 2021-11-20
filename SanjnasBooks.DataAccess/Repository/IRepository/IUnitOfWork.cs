427f43d3d7ec6812ac4993bef83f7d924b24b89a
Sanjna0769043 <W0769043@myscc.ca>
2021-11-20 14:41:18 -0500

    add the covertype in IUnitOfWorl file

--- SanjnasBooks.DataAccess/Repository/IRepository/IUnitOfWork.cs
+++ SanjnasBooks.DataAccess/Repository/IRepository/IUnitOfWork.cs
@@ -9,6 +9,7 @@ namespace SanjnasBooks.DataAccess.Repository.IRepository
     public interface IUnitOfWork : IDisposable
     {
         ICategoryRepository Category { get; }
+        ICoverTypeRepository CoverType { get; }
 
         ISP_Call SP_Call { get; }
 
