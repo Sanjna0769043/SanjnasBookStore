d971680a06a9dc4aa3f7fda5325ef375827bff19
Sanjna0769043 <W0769043@myscc.ca>
2021-11-20 15:22:37 -0500

    do changes in CoverTypeController

--- SanjnasBookStore/Areas/Admin/Controllers/CoverTypeController.cs
+++ SanjnasBookStore/Areas/Admin/Controllers/CoverTypeController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using SanjnasBooks.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -6,11 +7,84 @@ using System.Threading.Tasks;
 
 namespace SanjnasBookStore.Areas.Admin.Controllers
 {
-    public class CoverTypeController : Controller
-    {
-        public IActionResult Index()
+    
+        [Area("Admin")]
+        public class CoverTypeController : Controller
         {
-            return View();
+            private readonly IUnitOfWork _unitOfWork;
+
+            public CoverTypeController(IUnitOfWork unitOfWork)
+            {
+                _unitOfWork = unitOfWork;
+            }
+            public IActionResult Index()
+            {
+                return View();
+            }
+            public IActionResult Upsert(int? id)    //action method for Upsert
+            {
+            CoverType coverType = new CoverType();
+                if (id == null)
+                {
+                    // this is for create
+                    return View(coverType);
+                }
+
+            //this for the edit
+            coverType = _unitOfWork.CoverType.Get(id.GetValueOrDefault());
+                if (coverType == null)
+                {
+                    return NotFound();
+                }
+                return View();
+            }
+
+            //use HTTP POST to define the post-action method
+            [HttpPost]
+            [ValidateAntiForgeryToken]
+
+            public IActionResult Upsert(CoverType coverType)
+            {
+                if (ModelState.IsValid)     // checks all validations in the model
+                {
+                    if (coverType.Id == 0)
+                    {
+                        _unitOfWork.Category.Add(coverType);
+
+                    }
+                    else
+                    {
+                        _unitOfWork.Category.Update(coverType);
+                    }
+                    _unitOfWork.Save();
+                    return RedirectToAction(nameof(Index)); // to see all the categories
+
+
+                }
+                return View(coverType);
+            }
+
+            #region API CALLS
+            [HttpGet]
+            public IActionResult GetAll()
+            {
+                var allObj = _unitOfWork.Category.GetAll();
+                return Json(new { data = allObj });
+            }
+           /* [HttpDelete]
+            public IActionResult Delete(int id)
+            {
+                var objFromDb = _unitOfWork.Category.Get(id);
+                if (objFromDb == null)
+                {
+                    return Json(new { sucess = false, message = "Error while deleting " });
+                }
+                _unitOfWork.Category.Remove(objFromDb);
+                _unitOfWork.Save();
+                return Json(new { sucess = true, message = "Delete successful" });
+            } */
+
+            #endregion
         }
+
     }
-}
