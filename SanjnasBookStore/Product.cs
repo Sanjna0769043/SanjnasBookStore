29271278f02583349cc2ebd4d38498c779fc531d
Sanjna0769043 <W0769043@myscc.ca>
2021-11-20 16:40:19 -0500

    Typed the given code in side the Product.cs class

--- SanjnasBookStore/Product.cs
+++ SanjnasBookStore/Product.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using SanjnasBooks.Models;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,5 +10,25 @@ namespace SanjnasBookStore
 {
     public class Product
     {
+        [Key]
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string ISBN { get; set; }
+
+        [Required]
+        [Range(1, 10000)]
+        public double ListPrice { get; set; }
+        public string ImageUrl { get; set; }
+
+        [Required]
+        public int CategoryId { get; set; }   //foreign key refrance
+        [ForeignKey("CategoryId")]
+        public Category Category { get; set; }
+
+        [Required]
+        public int CoverTypeId { get; set; }
+        [ForeignKey("CategoryId")]
+        public CoverType CoverType { get; set; }
     }
 }
