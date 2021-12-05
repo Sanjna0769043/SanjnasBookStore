51e814eb7dcaef94245fcbb3715f4ffb15414e2b
Sanjna0769043 <W0769043@myscc.ca>
2021-12-05 12:11:44 -0500

    created new folder product under the new folder image

--- SanjnasBook.Utility/SD.cs
+++ SanjnasBook.Utility/SD.cs
@@ -8,5 +8,10 @@ namespace SanjnasBook.Utility
 {
      public static class SD
     {
+        public const string Proc_Cover_Create = "usp_CreateCoverType";
+        public const string Proc_Cover_Get = "usp_GetCoverType";
+        public const string Proc_Cover_GetAll = "usp_GetCoverType";
+        public const string Proc_Cover_Update = "usp_UpdateCoverType";
+        public const string Proc_Cover_Delete = "usp_DeleteCoverType";
     }
 }
